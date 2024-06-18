c3cc8e2305cefc66500798848f24c52c58e9fb57
Gabriel Santos <gabrielthss@gmail.com>
2024-06-18 18:34:49 -0300

    Criado em 11/06/2024. Atualizado em 18/06/2024. Em construção.

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,9 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
+
 
 public class Principal {
 
@@ -16,6 +20,13 @@ public class Principal {
         favorito.avalia(8);
         favorito.avalia(9);
 
+        Filme outroFilme = new Filme();
+
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+        outroFilme.setIncluidoNoPlano(true);
+
         System.out.println("Média de avaliações do filme: " +favorito.pegaMedia());
 
         Serie lost = new Serie();
@@ -24,8 +35,22 @@ public class Principal {
         lost.exibeFichaTecnica();
         lost.setTemporadas(10);
         lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(30);
         System.out.println("Tempo para maratonar a Série: " + lost.getDuracaoEmMinutos());
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(favorito);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println("Tempo total (em minutos): " + calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(favorito);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVizualizacoes(300);
+        filtro.filtra(episodio);
     }
-
-
 }
